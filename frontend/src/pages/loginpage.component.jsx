d48ab56d65b27f1bda420830cad12b52ff66de4a
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-05-05 13:09:25 +0800

    Set up template routing for Django and created LoginPage component

---
+++ frontend/src/pages/loginpage.component.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Container } from '@material-ui/core';
+import './loginpage.styles.scss'
+
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Container className = 'login-container'> Test</Container>
+        )
+
+    }
+
+}
+
+export default LoginPage;
