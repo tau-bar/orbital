4f97f234709a0e84989e139be58548df4f98c1ac
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-05-22 16:07:25 +0800

    For Reyaaz to fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Hamburger from './components/hamburger/hamburger.component';
 import { Route, Switch } from 'react-router-dom';
 import SignUpPage from './pages/login-page/signuppage.component';
 import NewVirusPage from './pages/new-virus-page/new-virus.component';
+import VirusPage from './pages/virus-page/virus-page.component';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path = "/" component = {HomePage}/>
         <Route exact path = "/login" component = {LoginPage}/>
         <Route exact path = "/sign-up" component = {SignUpPage}/>
+        <Route exact path = "/virus" component = {VirusPage} ></Route>
         <Route exact path = "/virus/create" component = {NewVirusPage}></Route>
       </Switch>
     </div>
