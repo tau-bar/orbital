8c4754f79ccd9b8bb34af5f905ffd634d54bbf3d
Muhammad  Reyaaz <mohdreyaaz99@gmail.com>
2021-07-26 10:42:28 +0800

    final push

--- src/components/homepage-timeline/homepage-timeline.component.jsx
+++ src/components/homepage-timeline/homepage-timeline.component.jsx
@@ -12,7 +12,7 @@ const timelinedata = {
         year: "1346",
         year2: "1352",
         deaths: "75-200 million",
-        content: "The plague that caused the Black Death originated in China in the early to mid-1300s and spread along trade routes westward to the Mediterranean and northern Africa. It reached southern England in 1348 and northern Britain and Scandinavia by 1350."
+        content: "Black Plague is also known as the bubonic plague or black death. It wiped out at least a third of the Europe population. On October 1347, 12 ships dropped anchor at the Sicilian port, Europe Italy. Before it even docked at Europe, rumours were already going around that this disease was already present in India, China, and Egypt. Hence, they concluded that it spread through trade routes. Black Plague spreads through contact of clothes, air, and pests such as rats. Theory suggests that back plague occurred in the same time as anthrax. Bloodletting, boil lancing and emersion of the sick in rose water or vinegar caused the spread to accelerate. Only a week is required before the infected person dies. 30% of Europe (50-70 million) and 90% of Florence died."
     },
     cholera: {
         name: "Cholera",
@@ -23,7 +23,7 @@ const timelinedata = {
         year: "1817",
         year2: "1824",
         deaths: "1-2 million",
-        content: "Cholera is an acute diarrhoeal infection caused by ingestion of food or water contaminated with the bacterium Vibrio cholerae. Cholera remains a global threat to public health and an indicator of inequity and lack of social development."
+        content: "Cholera is caused by a bacterium called Vibrio Cholerae, present in unsanitary waters. Once infected, the individual will suffer extreme diarrhoea, severe dehydration, and nausea. This leads to loss of large amounts of fluids from the body. The first recorded case of cholera infection is from India in 1536. 2.4 billion people in poor countries and villages cannot afford water filtration, leading to consumption of water contaminated with faeces. As a result, 100000 people from these areas die from cholera infection. The obvious treatment is to install basic water filtration in these regions, at a low cost. Sari filtration is first used in Bangladesh as a cost-effective filtration. By 2030, the World Health Organisation aims for Cholera infections to be eliminated from 20 countries. Also, death rate due to cholera is predicted to be reduced by 90%. Funds for the filtration will be managed by Global Task Force on Cholera Control (GTFCC)."
     },
     spanishFlu: {
         name: "Spanish Flu",
@@ -34,7 +34,7 @@ const timelinedata = {
         year: "1918",
         year2: "1920",
         deaths: "50 million",
-        content: "Spanish Flu is caused by a virus from the H1N1 family. It has a few nicknames, namely Kansas flu and the forgotten flu (because it happened in the same time as world war 1). Spanish flu ended in the mid 1919s. On March 11 1918, at Fort Riley, Albert Gitchell was the first person in the US to be infected with the Spanish flu. Since he was a cook, he came in close contact with many other soldiers there. It was agreed upon that Spanish flu started in China, Austria, or East Asia. Spanish flu name came about because during world war 1, Spain newspapers were not heavily censored and thus it seemed that the number of cases of the flu is drastically more in Spain than in other parts of the world. The total death toll was 27% of the world (50-100 million). From the US, 2 million people died. Among those 2 million that died, many of them were young adults who have strong immune systems. 17 million people died in India, 1.5 million people died in the Dutch East, 390,000 people died in Japan, 250 000 died in "
+        content: "Spanish Flu is caused by a virus from the H1N1 family. It has a few nicknames, namely Kansas flu and the forgotten flu (because it happened in the same time as world war 1). Spanish flu ended in the mid 1919s. On March 11 1918, at Fort Riley, Albert Gitchell was the first person in the US to be infected with the Spanish flu. Since he was a cook, he came in close contact with many other soldiers there. It was agreed upon that Spanish flu started in China, Austria, or East Asia. Spanish flu name came about because during world war 1, Spain newspapers were not heavily censored and thus it seemed that the number of cases of the flu is drastically more in Spain than in other parts of the world. The total death toll was 27% of the world (50-100 million). From the US, 2 million people died. Among those 2 million that died, many of them were young adults who have strong immune systems. 17 million people died in India, 1.5 million people died in the Dutch East, 390,000 people died in Japan, 250 000 died in Great Britain, 400 000 people died in France and 2 million people died in Iran."
     },
     sars: {
         name: "SARS",
@@ -64,7 +64,7 @@ const timelinedata = {
         year: "2019",
         year2: "now",
         deaths: "3.95 million",
-        content: "Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment."
+        content: "Cronavirus is also known as Viral Pneumonia or the novel virus. Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment. It can live on cardboard for 24 hours, steel for 48 hours and plastic for 72 hours. Since it is a novel virus, no human has a natural immunity agaisnt it"
     }
     
 }
